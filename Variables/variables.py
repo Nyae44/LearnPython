3c1120433beb70bf9ae13c4d9a60ec419f58948c
Nyae44 <tingadaley@gmail.com>
2024-01-25 14:53:21 +0300

    add simple variableprogram

---
+++ Variables/variables.py
@@ -0,0 +1,21 @@
+my_dog = 2
+my_dog = my_dog + my_dog
+print(my_dog)
+my_dog = ["Jiji", "Pipi"]
+print(my_dog)
+print(type(my_dog))
+""" 
+Python uses dynamic typing meaning you can reassign variables 
+to different data types 
+
+Rules for variable assignments 
+1. Names cannot start with a number 
+2. There should be no spaces in variable names instead use a _ 
+3. Variable names should not use any symbols
+4. Its considered best practice to use lowercase when naming variables
+5. Avoid using words that have a special meanng in Python
+"""
+my_income = 100
+my_tax_rate = 0.1
+taxes = my_income * my_tax_rate
+print(taxes)
