dfddd4916fac6328e23473742655673e4387a68d
Nyae44 <tingadaley@gmail.com>
2024-01-28 10:48:30 +0300

    working with booleans

---
+++ Booleans/booleans.py
@@ -0,0 +1,17 @@
+# Booleans are operators that allow you to convey true or false statements 
+# Booleans are important when dealing with control flow and logic 
+# Boolean values need to be capitalized for the interpreter to understand i.e True, False 
+# Booleans are used extensively with comparison operators
+True
+False
+# Check the type using the type() function
+
+print(type(True))
+
+# We can assign a none data type to a varible we want to use later on so as to avoid syntax errors
+b = None
+# We do not get an error whenever we are calling b 
+b  
+# We can check the datatype of b
+print(type(b)) # Returns NoneType
+
