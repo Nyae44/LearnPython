6fdb2b3dd552a9fa290efdb46655195af4d2359e
Nyae44 <tingadaley@gmail.com>
2024-01-28 13:12:19 +0300

    Working with files

--- Files/files.py
+++ Files/files.py
@@ -13,3 +13,16 @@ print(myfile.mode)
 
 # We have to explicitly close the file 
 myfile.close()
+# Using the with statement to read files
+with open('/home/nyae/LearnPython/LearnPython/Files/basic.txt', mode= 'r') as newfile:
+    contents = newfile.read()
+    print(contents)
+# How to add a new line to the file
+    with open('/home/nyae/LearnPython/LearnPython/Files/basic.txt', mode='a') as addline:
+        content_data = addline.write('\n this is the fourth line')
+        print(content_data)
+# How to overwrite an existing file or creating a new file 
+    with open('/home/nyae/LearnPython/LearnPython/Files/new.txt') as f:
+        latest = f.write('I created this file')
+        print(latest)
+        # I get a traceback error which i'll figure out later, I'm hungry
