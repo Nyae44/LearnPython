6243f7f39e5e1f289d34c12c4caeff5679ceded4
Nyae44 <tingadaley@gmail.com>
2024-02-18 23:54:45 +0300

    Implement hand branch

--- OOP_Project/challenge_project.py
+++ OOP_Project/challenge_project.py
@@ -5,7 +5,7 @@ import random
 
 suits = ('Hearts','Diamonds','Spades','Clubs')
 ranks = ('Two', 'Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
-values = {'Two':2, 'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'Queen':10,'Ace':11}
+values = {'Two':2, 'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'Queen':10,'King':10,'Ace':11}
 playing = True
 
 # Class definitions 
@@ -37,7 +37,38 @@ class Deck():
         single_card = self.deck.pop()
         return single_card
     
-# Testing
+# Testing Deck
 
 test_deck = Deck()
 print(test_deck)
+
+
+# Hand class -> represents what cards are in someone's hand 
+class Hand():
+    
+    def __init__(self):
+        self.cards = [] # starts with an empty list like in the Deck class 
+        self.value = 0 # start with zero value
+        self.aces = 0 # add an attribute to keep track of aces 
+    
+    def add_card(self,card):
+        # card passed in from Deck 
+        # from Deck.deal() -> suit,rank
+        self.cards.append(card)
+        self.value += values[card.rank]
+    def adjust_for_ace(self):
+        pass
+
+# testing 
+
+test_deck_ = Deck()
+test_deck_.shuffle()
+
+# Player
+test_player = Hand()
+# Deal 1 card pulled from the Deck(suit,rank)
+pulled_card = test_deck_.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.value)
+    
